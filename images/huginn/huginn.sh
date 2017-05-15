04059cae614e3523901331a0539d4e0155bc5e1f
Judi <judi0713@sina.com>
2017-05-15 16:54:58 +0800

    Update huginn.sh

--- images/huginn/huginn.sh
+++ images/huginn/huginn.sh
@@ -2,9 +2,19 @@ sudo apt-get update
 
 sudo apt-get -y install libmysqlclient-dev
 
-rvm install 2.3.3
+echo "Install RVM"
+echo "---------------------------------------------------------------------------"
 
+apt-get -y install nodejs
+
+command gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+command curl -sSL https://get.rvm.io | bash -s stable
+source /etc/profile.d/rvm.sh
+
+rvm install 2.3.3
 rvm use 2.3.3 --default
+rvm -v
+ruby -v
 
 sudo locale-gen en_US.UTF-8
 
