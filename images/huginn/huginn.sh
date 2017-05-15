40904823727917eb09bcd9b52f6f6a82236c4db9
Judi <judi0713@sina.com>
2017-05-15 17:17:09 +0800

    Update huginn.sh

--- images/huginn/huginn.sh
+++ images/huginn/huginn.sh
@@ -11,6 +11,8 @@ command gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A17031
 command curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 
+source ~/.rvm/scripts/rvm
+
 rvm install 2.3.3
 rvm use 2.3.3 --default
 rvm -v
